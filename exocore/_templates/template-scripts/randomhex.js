4688f98d04596c819a141d3ee814ce749fc66eeb
hot-domme <74973526+hot-domme@users.noreply.github.com>
2022-10-20 18:32:51 +1100

    vault backup: 2022-10-20 18:32:51

--- exocore/_templates/template-scripts/randomhex.js
+++ exocore/_templates/template-scripts/randomhex.js
@@ -1,4 +1,7 @@
 function my_function (msg) {
-    return "Message from my script:";
+    let characters = "0123456789abcdef"
+    let str = ""
+    for(let i = 0; i < 40; i++){
+        str += characters[Math.floor(Math.random() * 16)]
 }
 module.exports = my_function;
