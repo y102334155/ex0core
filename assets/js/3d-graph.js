ed1fe2b0cd38758d35020697029ee2e8d6688b79
hot-domme <74973526+hot-domme@users.noreply.github.com>
2023-04-21 22:20:58 +1000

    auto

--- assets/js/3d-graph.js
+++ assets/js/3d-graph.js
@@ -1,5 +1,7 @@
-// const distance = 500;
-  // const GROUPS = 12;
+const distance = 100;
+//===============================================================//
+// Function for linebreaking longer titles: 
+//===============================================================//
 const checkLineBreak = (text, words, symbol) => {
   let resultText = text.split(" ");
   if (!words) {
@@ -8,7 +10,6 @@ const checkLineBreak = (text, words, symbol) => {
   if (!symbol) {
     symbol = "\n";
   }
-
   if (resultText.length > words) {
     for (let i = 1; i < resultText.length; i++) {
       if (i % words == 0) {
@@ -21,48 +22,50 @@ const checkLineBreak = (text, words, symbol) => {
   resultText = resultText.join(" ");
   return resultText;
 };
-  const Graph = ForceGraph3D()
+
+//===============================================================//
+// FILTER OUT NODEW WITH NO NEIGHBOURS
+//===============================================================//
+// const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+
+// const result = words.filter(word => word.length > 6);
+
+const links = [];
+
+
+//===============================================================//
+// INITIALISE GRAPH, DEFINE SOURCE JSON, SET SIZE
+//===============================================================//
+const Graph = ForceGraph3D()
+
     (document.getElementById('3d-graph'))
-      // .onBackgroundClick(() => Graph.cameraPosition({ x: 10, y: 10, z: 10 }, { x: 1, y: 1, z: 1 }, 3000  ))
       .jsonUrl('/assets/graph-net-web.json')
       .height(850)
       .width(1700)
-      .onNodeClick(node => {
-        // Aim at node from outside it
-        const distance = 100;
-        const distRatio = 1 + distance/Math.hypot(node.x, node.y, node.z);
-    
-        const newPos = node.x || node.y || node.z
-          ? { x: node.x * distRatio, y: node.y * distRatio, z: node.z * distRatio }
-          : { x: 0, y: 0, z: distance }; // special case if node is in (0,0,0)
-    
-        Graph.cameraPosition(
-          newPos, // new position
-          node, // lookAt ({ x, y, z })
-          2000  // ms transition duration
-        );
-      })
-      .enableNodeDrag(true)
+      .cameraPosition({ z: distance })
+      .nodeId('id')
+//===============================================================//
+// SETTINGS AND VARIABLES
+//===============================================================//
+      // .enableNodeDrag(true)
       .linkWidth(0.1)
       .backgroundColor('black')	
-      .linkCurvature(0)
+      // .linkCurvature(0.1)
       .linkOpacity(1)
       .nodeOpacity(0.75)
-      // .linkDirectionalParticles(2)
-      // .linkDirectionalParticleSpeed(0.005)
+      .linkDirectionalParticles(2)
+      .linkDirectionalParticleSpeed(0.002)
       // .nodeLabel('label')
-      .enableNavigationControls(true)
+      // .enableNavigationControls(true)
       .showNavInfo(false)
-      .nodeVal(0.01)
-      // .nodeColor('red')
-      .nodeAutoColorBy('label')
+      .nodeVal(0.03)
+      .nodeColor('red')
+      // .nodeAutoColorBy('label')
       // .nodeVisibility('neighbours')
       // .nodeVisibility((node) => node.neighbours > Number("0") ? false : true)
-      // .cameraPosition({ z: distance })
       // .linkHoverPrecision(10)
       // .enablePointerInteraction(true)
-      // .nodeLabel('label')
-      
+
       .nodeThreeObjectExtend(true)
       .nodeThreeObject(node => {
         const sprite = new SpriteText(checkLineBreak(node.label), 5);
@@ -76,14 +79,60 @@ const checkLineBreak = (text, words, symbol) => {
         return sprite;
       });
 
-// Spread nodes a little wider
-      // Graph.d3Force('charge').strength(-60);
-  
-  // let angle = 0;
-  //   setInterval(() => {
-  //     Graph.cameraPosition({
-  //       x: distance * Math.sin(angle),
-  //       z: distance * Math.cos(angle)
-  //     });
-  //     angle += Math.PI / 1000;
-  //   }, 10);
+
+
+//===============================================================//
+// FORCES:
+//===============================================================//
+      // Graph.d3force('charge').forceManyBody();
+      // Graph.d3force('link').forceLink(links);
+      // Graph.d3force('center').strength(1000);
+      Graph.d3Force('charge').strength(-20);
+
+      
+//===============================================================//
+// ZOOM ON NODE CLICK
+//===============================================================//
+Graph.onNodeClick(node => {
+  // Aim at node from outside it
+  const distance = 100;
+  const distRatio = 1 + distance/Math.hypot(node.x, node.y, node.z);
+
+  const newPos = node.x || node.y || node.z
+    ? { x: node.x * distRatio, y: node.y * distRatio, z: node.z * distRatio }
+    : { x: 0, y: 0, z: distance }; // special case if node is in (0,0,0)
+
+  Graph.cameraPosition(
+    newPos, // new position
+    node, // lookAt ({ x, y, z })
+    2000  // ms transition duration
+  );
+})
+
+//===============================================================//
+// AUTOMATIC ORBIT
+//===============================================================//
+
+let isRotationActive = false;
+let angle = 0;
+  setInterval(() => {
+    if (isRotationActive) {
+      Graph.cameraPosition({
+        x: distance * Math.sin(angle),
+        z: distance * Math.cos(angle)
+     });
+      angle += Math.PI / 1000;
+    }
+   }, 10);
+
+
+
+//===============================================================//
+// PAUSE ROTATION BUTTON
+//===============================================================//
+document.getElementById('rotationToggle').addEventListener('click', event => {
+  isRotationActive = !isRotationActive;
+  event.target.innerHTML = `${(isRotationActive ? 'Pause' : 'Orbit')}`;
+}); 
+
+
